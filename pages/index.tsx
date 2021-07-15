acb994ffbd5e646c15ce753cc3ab70d69cd7540d
Ahumuza <lizahumuza@gmail.com>
2021-07-15 12:11:00 +0300

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import {background, Button, Flex, Heading, Input, useColorMode, useColorModeValue} from '@chakra-ui/react'
 import Link from 'next/link'
-import AboutPage from '../components/aboutPage'
+
 
 const IndexPage = () =>{ 
   const {toggleColorMode} = useColorMode()
-  const formBackground = useColorModeValue("gray.100", "gray.700")
+  const formBackground = useColorModeValue("gray.300", "gray.700")
   return(
   <Flex height="100vh" alignItems="center" justifyContent="center" >
     <Flex direction="column" background={formBackground}  p={12} rounded={6}>
